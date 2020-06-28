df24866ca662506074765ba19df2950ed3c10271
pawdyw <pawdyw@LAPTOP-JHQ9SF1E>
2020-06-28 10:10:09 +0200

    Add project files.

---
+++ CollectIt.Tests/ListTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CollectIt.Tests
+{
+    [TestClass]
+    public class ListTests
+    {
+        [TestMethod]
+        public void A_List_Can_Insert()
+        {
+            List<int> integers = new List<int> { 1, 2, 3 };
+            integers.Insert(1, 6);
+
+            Assert.AreEqual(6, integers[1]);
+        }
+
+        [TestMethod]
+        public void A_List_Can_Remove()
+        {
+            List<int> integers = new List<int> { 1, 2, 3 };
+            integers.Remove(2);
+
+            Assert.IsTrue(integers.SequenceEqual(new[] { 1, 3 }));
+        }
+
+        [TestMethod]
+        public void A_List_Can_Find_Things()
+        {
+            List<int> integers = new List<int> { 1, 2, 3 };
+
+            Assert.AreEqual(2, integers.IndexOf(3));
+        }
+    }
+}
---
+++ CollectIt/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessEmployeeArray();
+
+            WriteSeparator();
+
+            ProcessEmployeeList();
+
+            WriteSeparator();
+
+            try
+            {
+                ProcessIntList();
+            }
+            catch { }
+
+            WriteSeparator();
+
+            ProcessEmployeeQueue();
+
+            WriteSeparator();
+
+            ProcessEmployeeStack();
+
+            Console.ReadLine();
+        }
+
+        private static void ProcessEmployeeArray()
+        {
+            Employee[] employees = new Employee[]
+            {
+                new Employee { Name = "Scott" },
+                new Employee { Name = "Alex" }
+            };
+
+            foreach (var employee in employees)
+            {
+                Console.WriteLine(employee.Name);
+            }
+
+            for (int i = 0; i < employees.Length; i++)
+            {
+                Console.WriteLine(employees[i].Name);
+            }
+
+            Array.Resize(ref employees, 10);
+        }
+
+        private static void ProcessEmployeeList()
+        {
+            List<Employee> employees = new List<Employee>
+            {
+                new Employee { Name = "Scott" },
+                new Employee { Name = "Alex" }
+            };
+
+            employees.Add(new Employee { Name = "Dani" });
+
+            foreach (var employee in employees)
+            {
+                Console.WriteLine(employee.Name);
+            }
+
+            for (int i = 0; i < employees.Count; i++)
+            {
+                Console.WriteLine(employees[i].Name);
+            }
+        }
+
+        private static void ProcessIntList()
+        {
+            var numbers = new List<int>();
+            var capacity = -1;
+
+            while (true)
+            {
+                if (numbers.Capacity != capacity)
+                {
+                    capacity = numbers.Capacity;
+                    Console.WriteLine(capacity);
+                }
+
+                numbers.Add(1);
+            }
+        }
+
+        private static void ProcessEmployeeQueue()
+        {
+            Queue<Employee> queue = new Queue<Employee>();
+            queue.Enqueue(new Employee { Name = "Alex" });
+            queue.Enqueue(new Employee { Name = "Dani" });
+            queue.Enqueue(new Employee { Name = "Chris" });
+
+            while (queue.Count > 0)
+            {
+                var employee = queue.Dequeue();
+                Console.WriteLine(employee.Name);
+            }
+        }
+
+        private static void WriteSeparator()
+        {
+            Console.WriteLine("---");
+        }
+
+        private static void ProcessEmployeeStack()
+        {
+            Stack<Employee> stack = new Stack<Employee>();
+            stack.Push(new Employee { Name = "Alex" });
+            stack.Push(new Employee { Name = "Dani" });
+            stack.Push(new Employee { Name = "Chris" });
+
+            while (stack.Count > 0)
+            {
+                var employee = stack.Pop();
+                Console.WriteLine(employee.Name);
+            }
+        }
+    }
+}
